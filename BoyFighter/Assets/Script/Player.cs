e9dcc1b18463ff6f7075605b78cad8bc2bbc84e5
Jay <mhanhxv@gmail.com>
2020-04-03 17:51:42 -0400

    Movement and jump
    Main character able to move and jump

--- BoyFighter/Assets/Script/Player.cs
+++ BoyFighter/Assets/Script/Player.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 50f, maxSpeed=3f, jumpPow = 220f;
+    public bool grounded = true, faceright = true;
 
-    public Rigidbody2D boy;    
+    public Rigidbody2D boy;  
+    public Animator anim;  
     // Start is called before the first frame update
     void Start()
     {
         boy = gameObject.GetComponent<Rigidbody2D>();
-
+        anim = gameObject.GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(boy.velocity.x));
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (grounded){
+                grounded=false;
+                boy.AddForce(Vector2.up * jumpPow);
+            }
+        }
     }
 
     void FixedUpdate() {
@@ -31,5 +41,20 @@ public class Player : MonoBehaviour
          if (boy.velocity.x < -maxSpeed){
             boy.velocity = new Vector2(-maxSpeed, boy.position.y);
         }
+        if(h>0 && !faceright){
+            Flip();
+        }
+        if(h<0 && faceright){
+            Flip();
+        }
+    }
+
+    public void Flip(){  
+        faceright = !faceright;
+        Vector3 Scale;
+        Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+        
     }
 }
